fbc32f11e741a37b93af524fcb978c20a66a144f
Gurjaka <esartia.gurika@gmail.com>
2024-09-18 22:37:38 +0400

    C-Learning: Max-of-four.c

---
+++ max-of-four.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+  int numbers[4];
+  for (int i = 0; i < 4; i++) {
+    printf("Num%d\n", i+1);
+    scanf("%d", &numbers[i]);
+  }
+  int max = numbers[0];
+  for (int i = 1; i < 4; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  printf("\nMax is: %d", max);
+  return 0;
+}
