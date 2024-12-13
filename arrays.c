501b80eafe6c7ce17c9dd08b3651324a6eeab4ac
Gurjaka <esartia.gurika@gmail.com>
2024-12-13 17:00:34 +0400

    Functions

--- arrays.c
+++ arrays.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
   int c, i, nwhite, nother;
   int ndigit[10];
 
   nwhite = nother = 0;
-  for (i = 0; i < 10; ++i) {
+  for (i = 0; i < 10; ++i)
     ndigit[i] = 0;
-  }
 
   while ((c = getchar()) != EOF) {
-    if (c >= '0' && c <= '9') {
+    if (c >= '0' && c <= '9') 
       ++ndigit[c-'0'];
-    } else if (c == ' ' || c == '\n' || c == '\t') {
+
+    else if (c == ' ' || c == '\n' || c == '\t') 
       ++nwhite;
-    } else {
+
+    else
       ++nother;
-    }
   }
 
-  for (i = 0; i < 10; ++i) {
+  for (i = 0; i < 10; ++i) 
     printf("digits = %d", ndigit[i]);
-  }
 
   printf(", white space = %d, other = %d \n", nwhite, nother);
 
