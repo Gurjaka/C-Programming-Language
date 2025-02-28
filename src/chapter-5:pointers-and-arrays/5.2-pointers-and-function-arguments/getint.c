f8f25d66c2d300bb498c6163b4abf7bc926d6b19
Gurjaka <esartia.gurika@gmail.com>
2025-02-28 20:34:47 +0400

    get_float

--- src/chapter-5:pointers-and-arrays/5.2-pointers-and-function-arguments/getint.c
+++ src/chapter-5:pointers-and-arrays/5.2-pointers-and-function-arguments/getint.c
@@ -1,53 +1,79 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 static char buffer_char = 0;
-static int buffer_flag = 0;
+static bool buffer_flag = false;
 
+// get_char: returns the next character, considering buffered characters.
 int get_char(void) {
   if (buffer_flag) {
-    buffer_flag = 0;
+    buffer_flag = false;
     return buffer_char;
   }
   return getchar();
 }
 
+// put_char_back: puts a character back into the input stream for later use.
 void put_char_back(int c) {
-  if (buffer_flag)
-    printf("put_char_back: too many characters\n");
-  else {
+  if (buffer_flag) {
+    printf("Error: too many characters put back\n");
+  } else {
     buffer_char = c;
-    buffer_flag = 1;
+    buffer_flag = true;
   }
 }
 
-// get_integer: get next integer from input into *number
+// get_integer: reads the next integer from input into *number.
+// Returns 1 on success, 0 on failure.
 int get_integer(int *number) {
-  int current_char, sign;
+  int current_char, sign = 1;
 
+  // Skip whitespace characters.
   while (isspace(current_char = get_char()))
     ;
 
+  // Check if the character is a valid starting point for an integer.
   if (!isdigit(current_char) && current_char != EOF && current_char != '+' &&
       current_char != '-') {
     put_char_back(current_char);
     return 0;
   }
-  sign = (current_char == '-') ? -1 : 1;
-  if (current_char == '+' || current_char == '-')
+
+  // Handle sign of the number.
+  if (current_char == '-') {
+    sign = -1;
     current_char = get_char();
-  for (*number = 0; isdigit(current_char); current_char = get_char())
+  } else if (current_char == '+') {
+    current_char = get_char();
+  }
+
+  // Accumulate the integer value.
+  *number = 0;
+  while (isdigit(current_char)) {
     *number = 10 * *number + (current_char - '0');
+    current_char = get_char();
+  }
+
   *number *= sign;
-  if (current_char != EOF)
+
+  // If the next character is not EOF, put it back.
+  if (current_char != EOF) {
     put_char_back(current_char);
-  return current_char;
+  }
+
+  return 1;
 }
 
 int main(void) {
-  int number, result;
-  result = get_integer(&number);
-  printf("you entered %d", number);
+  int number;
+  printf("Enter an integer: ");
+  if (get_integer(&number)) {
+    printf("You entered %d\n", number);
+  } else {
+    printf("Error: Not a valid number\n");
+  }
+
   return EXIT_SUCCESS;
 }
